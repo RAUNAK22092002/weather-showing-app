5c02108973a7b90c523570e04dc7108f272a49e0
RAUNAK22092002 <76444444+RAUNAK22092002@users.noreply.github.com>
2022-04-04 15:35:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Weather from './Weather';
+import "./style.css";
+ 
+const App = () => {
+  return (
+    <div>
+        {/* const App
+         */}
+          <Weather />
+    </div>
+  )
+}
+
+export default App;
